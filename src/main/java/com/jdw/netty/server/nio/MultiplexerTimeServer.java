09c624b0d474964f17b99656e78efc02c295a01a
jiadawei <jiadawei@zehin.com.cn>
2016-07-20 17:00:28 +0800

    NIO服务端代码

--- src/main/java/com/jdw/netty/server/nio/MultiplexerTimeServer.java
+++ src/main/java/com/jdw/netty/server/nio/MultiplexerTimeServer.java
@@ -41,25 +41,73 @@ public class MultiplexerTimeServer implements Runnable {
 
 	@Override
 	public void run() {
-		try {
-			selector.select(1000);
-			Set<SelectionKey> selectedKeys=selector.selectedKeys();
-			Iterator<SelectionKey> it=selectedKeys.iterator();
-			SelectionKey key=null;
-			while(it.hasNext()){
-				key=it.next();
-				it.remove();
+		while (!stop) {
+			try {
+				selector.select(1000);
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> it = selectedKeys.iterator();
+				SelectionKey key = null;
+				while (it.hasNext()) {
+					key = it.next();
+					it.remove();
+					try {
+						handleInput(key);
+					} catch (Exception e) {
+						if (key != null) {
+							key.cancel();
+							if (key.channel() != null) {
+								key.channel().close();
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		}
+
+		if (selector != null) {
+			try {
+				selector.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
-	private void doWrite(SocketChannel channel,String response) throws IOException{
-		if(response!=null && response.trim().length()>0){
-			byte[] bytes=response.getBytes();
-			ByteBuffer writeBuffer=ByteBuffer.allocate(bytes.length);
+	private void handleInput(SelectionKey key) throws IOException {
+		if (key.isValid()) {
+			if (key.isAcceptable()) {
+				ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+				SocketChannel sc = ssc.accept();
+				sc.configureBlocking(false);
+				sc.register(selector, SelectionKey.OP_READ);
+			}
+
+			if (key.isReadable()) {
+				SocketChannel sc = (SocketChannel) key.channel();
+				ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+				int readBytes = sc.read(readBuffer);
+				if (readBytes > 0) {
+					readBuffer.flip();
+					byte[] bytes = new byte[readBuffer.remaining()];
+					readBuffer.get(bytes);
+					String body = new String(bytes, "UTF-8");
+					System.out.println("The time server receive order: " + body);
+					String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+							? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+					doWrite(sc, currentTime);
+				}
+			}
+		}
+	}
+
+	private void doWrite(SocketChannel channel, String response) throws IOException {
+		if (response != null && response.trim().length() > 0) {
+			byte[] bytes = response.getBytes();
+			ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
 			writeBuffer.put(bytes);
 			writeBuffer.flip();
 			channel.write(writeBuffer);
